161938ebd8d6b3d61565814a3de88a06ed52b871
mamen <mamen@ECE000065>
2022-11-09 17:25:44 +0200

    Add project files.

---
+++ Finalizer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finalizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateSomeGarbage();
+
+            Console.WriteLine($"Memory Befor Dispose {GC.GetTotalMemory(false):N0}");
+            GC.Collect();
+            Console.WriteLine($"Memory Befor Dispose {GC.GetTotalMemory(true):N0}");
+
+            Console.ReadLine();
+        }
+        static void CreateSomeGarbage()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Version version = new Version();
+            }
+        }
+    }
+
+
+}
